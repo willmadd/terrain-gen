7915c96ba74dfe4608979e9d48ad6dc2f6249caf
William <39624752+willmadd@users.noreply.github.com>
2023-07-26 21:53:43 +0100

    another section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,6 @@ export default function Home() {
             >
               Generate Terrain
             </a>
-            {/* <a
-        href={`/generate`}
-        className={
-          "bg-white mt-8 rounded-md shadow-md p-2 h-12 block flex items-center justify-center"
-        }
-      >
-        Generate Terrain
-      </a> */}
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
             <img
@@ -51,21 +43,38 @@ export default function Home() {
           </div>
         </div>
       </section>
-      {/* <div
-        className={"bg-white mt-4 rounded-md shadow-md p-2 h-12"}
-        > */}
-      {/* <a
-        href={`/generate`}
-        className={
-          "bg-white mt-8 rounded-md shadow-md p-2 h-12 block flex items-center justify-center"
-        }
-      >
-        Generate Terrain
-      </a> */}
-      {/* </div> */}
-      {/* </div> */}
+      <section className="bg-white dark:bg-gray-900 mt-12 px-8 rounded-xl">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className=" ml-auto place-self-center lg:col-span-7 order-2">
+            <h2 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              From rolling hills to alien landscapes
+            </h2>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Terrain Gen, your go-to destination for creating and downloading
+              low poly terrain models. Create virtual landscapes with ease using
+              our fully customisable terrain generation tool. Terrain Gen
+              enables you to create terrain models that are downloadable as GLB
+              files for free. Unleash your creativity and elevate your projects
+              by starting to design terrain today.
+            </p>
+
+            <a
+               href={`/generate`}
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              Generate Terrain
+            </a>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img
+              src="/images/icebergs.webp"
+              alt="mockup"
+            />
+          </div>
+        </div>
+      </section>
       <img
-        className=" absolute -z-10 object-cover h-full w-full"
+        className=" fixed -z-10 object-cover h-full w-full"
         src={"/images/terrain2.webp"}
         alt={"3d image of some low poly terrain"}
       />
